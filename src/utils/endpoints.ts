87d37418046c5f6ae5e02bd3e1cf410704cfd942
Vivek <vivekcse.in@gmail.com>
2023-11-19 11:14:00 +0530

    full stack starter template for nextjs14

---
+++ src/utils/endpoints.ts
@@ -0,0 +1,7 @@
+// export const endpoint_layout = 'http://localhost:3000/api/layout'
+// export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
+// export const endpoint_layout = 'https://vivekcsein.github.io/apidata/layout.json'
+// export const endpoint_layout = 'https://portfolio.vercel.app/api/'
+export const endpoint_layout = 'https://vivekcse.github.io/frnz/layout.json';
+
+export const hostingLink = 'https://vivekcse.github.io/frnz';
