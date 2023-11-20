d405415d5ca78b77dbe4aef549f15ec16cdf6ccb
Vivek <vivekcse.in@gmail.com>
2023-11-20 08:41:57 +0530

    endpoit changed

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,7 +1,8 @@
 // export const endpoint_layout = 'http://localhost:3000/api/layout'
-export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
+// export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
 // export const endpoint_layout = 'https://vivekcsein.github.io/apidata/layout.json'
 // export const endpoint_layout = 'https://portfolio.vercel.app/api/'
 // export const endpoint_layout = 'https://vivekcse.github.io/frnz/layout.json';
+export const endpoint_layout = 'https://https://frnz-m3ok0cukt-viveks-projects-13375c0a.vercel.app/api/layout.json'
 
 export const hostingLink = 'https://vivekcse.github.io/frnz';
