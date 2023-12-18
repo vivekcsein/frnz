03a9ebf3fd50255921440d9dbeb1e2b1c2248652
Vivek <vivekcse.in@gmail.com>
2023-12-19 02:26:58 +0530

    lets try again

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -4,8 +4,22 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
+let prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient()
+    process.on('SIGINT', () => prismaclose())
+    process.on('exit', () => prismaclose())
+} else {
+    globalForPrisma.prisma = prisma
+}
+async function prismaclose() {
+    if (globalForPrisma.prisma) {
+        await globalForPrisma.prisma.$disconnect()
+        delete globalForPrisma.prisma
+
+    }
+}
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export const db = prisma;
