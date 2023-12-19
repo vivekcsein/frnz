f482743425fdef033cf3c3a080f4ae5d0312bada
Vivek <vivekcse.in@gmail.com>
2023-12-19 16:11:26 +0530

    prisma changes has been reversed

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,4 +1,11 @@
-// export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
-export const endpoint_layout = 'http://frenzz.in/api/layout'
+export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
+// export const endpoint_layout = 'http://frenzz.in/api/layout'
 
 export const endpoint_toonify = 'https://toonify.p.rapidapi.com/v0/toonmojihd?proceed_without_face=false&return_aligned=false'
+
+
+// "postinstall": "prisma generate",
+// "vercel-build": "prisma generate && prisma migrate deploy && next build",
+//    "generate": "npx prisma generate",
+
+// "migrate": "npx prisma migrate dev --name init"
