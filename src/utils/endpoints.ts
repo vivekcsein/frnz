f114c95e124ecc4d37da614e960ab63a42b8e98b
Vivek <vivekcse.in@gmail.com>
2023-12-19 16:43:05 +0530

    Prisma is updated now

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,5 +1,5 @@
-export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
-// export const endpoint_layout = 'http://frenzz.in/api/layout'
+// export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
+export const endpoint_layout = 'http://frenzz.in/api/layout'
 
 export const endpoint_toonify = 'https://toonify.p.rapidapi.com/v0/toonmojihd?proceed_without_face=false&return_aligned=false'
 
